5a71bace9aa2efac6fc16c1610f24ede4a08ab2f
James Thomas <jamesjacobthomas7@gmail.com>
2018-03-15 21:03:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var arr = []
-  for (musicians = 0; musicians < 4; musicians++) {
+  for (musicians = 0; musicians < 3; musicians++) {
     arr.push(`${musicians} plays ${instruments} `)
   }
   return arr
