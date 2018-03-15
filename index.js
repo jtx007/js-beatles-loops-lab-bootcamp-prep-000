09727b6f33b8ef91db4fe0675cb73fd86969f6b6
James Thomas <jamesjacobthomas7@gmail.com>
2018-03-15 21:56:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function theBeatlesPlay(musicians, instruments) {
 
 function johnLennonFacts(facts) {
   let i = 0
-  while (i < facts.length) {
+  while (i < facts.length; i++) {
     facts[i] = `${facts[i]!!!}`
   }
 }
